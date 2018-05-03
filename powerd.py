2976ff8189bd6a5884f6dc90cae1f5ffa252f225
Akhil Guliani <guliani@wisc.edu>
2018-05-03 15:40:01 -0500

    energy tracker class and system stats function

--- powerd.py
+++ powerd.py
@@ -18,53 +18,105 @@ from docopt import docopt
 from schema import Schema, And, Or, Use, SchemaError
 import psutil
 import subprocess
+import time
+import signal
+import sys
 
+def signal_handler(signal, frame):
+    print('\nExiting Daemon')
+    # Do cleanup in the future
+    sys.exit(0)
 
 # Create a class for holding historic values to get differences
 # between intervals
-MAX_ENERGY = 262143328850
+class EnergyTracker:
+    """
+    Class energyTracker
+    Class designed to track the energy numbers provided by the powercap
+    kernel interface, currently only usable for intel RAPL
+    TODO extend the driver for use with AMD Ryzen, atleast the reporting api
+    """
 
-def getEnergy(prev_energy, init=False):
-    # Currenly using linux sysfs powercap interface to gather values
-    # for AMD that might mean adating the powercap driver (TODO)
-    # Can be substitued with MSR interface (Kernel Docs suggest to avoid)
+    MILLI = 1000
+    MAX_ENERGY = 262143328850
     socket = 0
-    domain = 0
+    energy = 0
+    share = 0
     energy_loc = "/sys/devices/virtual/powercap/intel-rapl/intel-rapl:"+str(socket)+"/"
-    energy_file = open(energy_loc+"energy_uj", 'r')
-    if init == True:
-        max_enrgy_file = open(energy_loc+"max_energy_range_uj", 'r')
-        MAX_ENERGY = int(max_enrgy_file.read())
+
+    def __init__(self, share):
+        max_enrgy_file = open(self.energy_loc+"max_energy_range_uj", 'r')
+        self.MAX_ENERGY = int(max_enrgy_file.read())
         max_enrgy_file.close()
-    energy = int(energy_file.read())
-    if energy < prev_energy:
-        return energy + MAX_ENERGY
-    else:
-        return energy
+        self.energy = self.getUpdateEnergy()
+        if share > 0 and share < 100:
+            self.share = share
+        else:
+            self.share = 0
+        self.author = "Akhil Guliani"
+        self.description = "Lets track some energy shall we"
+
+    def getUpdateEnergy(self):
+        # domain = 0
+        energy_file = open(self.energy_loc+"energy_uj", 'r')
+        energy = int(energy_file.read())
+        if energy < self.energy:
+            self.energy = energy + self.MAX_ENERGY
+        else:
+            self.energy = energy
+        energy_file.close()
+        return self.energy
+
+    def updateEnergy(self):
+        # domain = 0
+        energy_file = open(self.energy_loc+"energy_uj", 'r')
+        energy = int(energy_file.read())
+        if energy < self.energy:
+            self.energy = energy + self.MAX_ENERGY
+        else:
+            self.energy = energy
+        energy_file.close()
+
+    def getEnergy(self,share=100):
+        return self.energy
+
+    def getPower(self,prev_energy,interval=1):
+        """
+        return power in milli watts after interval
+        """
+        # ensure latest value is updated
+        self.updateEnergy()
+        # return power value in milliwats
+        return (self.energy-prev_energy)/(interval*self.MILLI)
+
 
 def getSysStats(cpu=None):
     """
     Get system stats current value
+    get global values or values for one cpu
     """
+    sys_stats = {}
     # get cputimes
-
+    sys_stats['time'] = psutil.cpu_times()
+    sys_stats['times'] = psutil.cpu_times(percpu=True)
     # get temperatures
-
+    sys_stats['temps'] = (psutil.sensors_temperatures())['coretemp']
     # get frequencies
-
-    # get energy
-    ## attribute energy to a core
-    energy = getEnergy(0,True)
+    sys_stats['freqs'] = psutil.cpu_freq(percpu=True)
 
     if cpu != None:
         # collect stats for particular cpu and return
-        return 0
-    else:
-        # return everything
-        return energy
+        cpu_stats = {}
+        cpu_stats['times'] = sys_stats['times'][cpu]
+        cpu_stats['freqs'] = sys_stats['freqs'][cpu]
+        cpu_stats['temps'] = sys_stats['temps'][cpu+1]
+        cpu_stats['time'] = sys_stats['time']
+        return cpu_stats
+    # return everything
+    return sys_stats
 
 
-def printProcess(_pids=['1']):
+def printProcess(_pids=None):
     """
     Iterate over all process and print proc vals
     """
@@ -84,24 +136,35 @@ def main(arg1):
     """
     print(arg1)
     printProcess(arg1['PID'])
-    print(getSysStats())
+    _ea = EnergyTracker(100)
+
+    while 1:
+        prev_energy = _ea.getUpdateEnergy()
+        time.sleep(int(arg1['--interval']))
+        #eA.updateEnergy()
+        print(_ea.getPower(prev_energy, int(arg1['--interval'])))
+        print(getSysStats(0))
 
 if __name__ == "__main__":
-    # execute only if run as a script
+    # Get Command line arguments
     ARGUMENTS = docopt(__doc__, version="0.01a")
 
+    # Check Command line arguments
     SCHEMA = Schema({
         '--input': Or(None, And(Use(open,
-                      error='input FILE should be readable'))),
+                                    error='input FILE should be readable'))),
         '--interval': Or(None, And(Use(int), lambda n: 0 < n < 1000),
-                        error='--interval=N should be integer 0 < N < 1000'),
-         'PID': [Or(None, And(Use(int), lambda n: 1 < n < 32768),
+                         error='--interval=N should be integer 0 < N < 1000'),
+        'PID': [Or(None, And(Use(int), lambda n: 1 < n < 32768),
                    error='PID should be inteager within 1 < N < 32768')],
-         })
+    })
     try:
-        arg_validator = SCHEMA.validate(ARGUMENTS)
+        ARG_VALIDATE = SCHEMA.validate(ARGUMENTS)
     except SchemaError as _e:
         exit(_e)
+
+    #Setup signal handler
+    signal.signal(signal.SIGINT, signal_handler)
 
     # Start main program here
     main(ARGUMENTS)
